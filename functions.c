5a7016dea66fa693e3af3df8ef644811bd93c136
root <albertorosillogarcia@gmail.com>
2021-11-09 14:30:01 -0800

    mitad de archivo

--- functions.c
+++ functions.c
@@ -12,30 +12,7 @@ int print_c(va_list args)
 	return (1);
 }
 
-
-/**
- * print_s - prints a string
- * @args: arguments received from _printf
- * Return: number of chars
- */
-int print_s(va_list args)
-{
-	int i;
-	char *str;
-
-	str = va_arg(args, char *);
-	if (str == NULL)
-	{
-		str = "(null)";
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		write(1, &str[i], 1);
-	}
-	return (i);
-}
-
-
+//print_s
 
 /**
  * print_i - print integers
@@ -85,37 +62,8 @@ int print_i(va_list args)
 	_putchar(b + '0');
 	return (bytes + cifras + 1);
 }
+//print_b
 
-/**
- * print_b - the unsigned int argument is converted to binary
- * @args: taking in arguments
- * Return: number of digits printed
- */
-int print_b(va_list args)
-{
-	unsigned int n, buff[1024];
-	int i, len = 0;
-	char p;
-
-	n = va_arg(args, int);
-	if (n < 1)
-	{
-		write(1, "0", 1);
-		return (1);
-	}
-	while (n > 0)
-	{
-		buff[len] = n % 2;
-		n /= 2;
-		len++;
-	}
-	for (i = len - 1; i >= 0; i--)
-	{
-		p = buff[i] + '0';
-		write(1, &p, 1);
-	}
-	return (len);
-}
 
 /**
  * print_o - the unsigned int argument is converted to octal
@@ -147,42 +95,9 @@ int print_o(va_list args)
 	}
 	return (len);
 }
-/**
- * print_x - unsigned int argument is converted to hexa in lowercase
- * @args: pointer to arguments
- * Return: number of digits printed
- */
+//print_x(minuscula)
 
-int print_x(va_list args)
-{
-	unsigned int n, buff[1024];
-	int i = 0, len = 0;
-	char p;
-
-	n = va_arg(args, int);
-	if (n < 1)
-	{
-		write(1, "0", 1);
-		return (1);
-	}
-	while (n > 0)
-	{
-		buff[len] = n % 16;
-		n /= 16;
-		if (buff[len] > 9)
-			buff[i] = buff[len] + 39;
-		else
-			buff[i] = buff[len];
-		i++;
-		len++;
-	}
-	for (i = len - 1; i >= 0; i--)
-	{
-		p = buff[i] + '0';
-		write(1, &p, 1);
-	}
-	return (len);
-}
+ 
 
 /**
  * print_X - unsigned int argument is converted to hexa in uppercase
@@ -246,45 +161,7 @@ int print_r(va_list args)
 	return (len);
 }
 
-/**
- * print_R - print a string to rot13 encode.
- * @args: Pointer to string.
- *
- * Return: lenght of the string encode in rot13
- */
-int print_R(va_list args)
-{
-	int i, j, len = 0;
-	char alpha[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
-	char ch[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
-	char *p, *m;
-
-	p = va_arg(args, char *);
-	for (i = 0; p[i] != '\0'; i++)
-		len++;
-	m = malloc(sizeof(char) * (len + 1));
-	if (!m)
-		return ('\0');
-	for (i = 0; i <= len; i++)
-	{
-		m[i] = p[i];
-	}
-	for (i = 0 ; m[i] != '\0' ; i++)
-	{
-		for (j = 0 ; alpha[j] != '\0' ; j++)
-		{
-			if (m[i] == alpha[j])
-			{
-				m[i] = ch[j];
-				break;
-			}
-		}
-	}
-	for (i = 0; m[i] != '\0'; i++)
-		write(1, &m[i], 1);
-	free(m);
-	return (len);
-}
+//print_R(mayuscula)
 /**
  * aux_func - selects the function to format the text
  *
